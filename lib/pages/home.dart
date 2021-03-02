c4baba1c1aefd0eede498d9b477bf9084355d665
chirag <chiragaparadh@gmail.com>
2021-03-02 15:30:33 +0530

    Updated home screen text.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,25 +19,23 @@ class _HomeState extends State<Home> {
       backgroundColor: bgColor,
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/$bgImage'),
-            fit: BoxFit.cover
-          )
-        ),
+            image: DecorationImage(
+                image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
         child: SafeArea(
             child: Padding(
           padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
           child: Column(
             children: [
               TextButton.icon(
-                onPressed: () async{
-                  dynamic result = await Navigator.pushNamed(context, '/location');
+                onPressed: () async {
+                  dynamic result =
+                      await Navigator.pushNamed(context, '/location');
                   setState(() {
                     data = {
-                      'time' : result['time'],
-                      'location' : result['location'],
-                      'flag' : result['flag'],
-                      'isDayTime' : result['isDayTime']
+                      'time': result['time'],
+                      'location': result['location'],
+                      'flag': result['flag'],
+                      'isDayTime': result['isDayTime']
                     };
                   });
                 },
@@ -45,7 +43,8 @@ class _HomeState extends State<Home> {
                   Icons.edit_location,
                   color: Colors.grey[200],
                 ),
-                label: Text('Edit Location', style: TextStyle(color: Colors.grey[200])),
+                label: Text('Change Location',
+                    style: TextStyle(color: Colors.grey[200])),
               ),
               SizedBox(height: 50),
               Row(
@@ -53,13 +52,17 @@ class _HomeState extends State<Home> {
                 children: [
                   Text(
                     data['location'],
-                    style: TextStyle(fontSize: 40, letterSpacing: 2.0, color: Colors.grey[200]),
+                    style: TextStyle(
+                        fontSize: 40,
+                        letterSpacing: 2.0,
+                        color: Colors.grey[200]),
                   ),
                 ],
               ),
               Text(
                 data['time'],
-                style: TextStyle(fontSize: 66, letterSpacing: 2, color: Colors.grey[200]),
+                style: TextStyle(
+                    fontSize: 66, letterSpacing: 2, color: Colors.grey[200]),
               ),
             ],
           ),
